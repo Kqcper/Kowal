3a1a4af731bc833e7c04f88567e4844e1de4319e
Kqcper <123020297+Kqcper@users.noreply.github.com>
2023-12-18 12:09:55 +0100

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Nice' },
+  { id: 13, name: 'Max' },
+  { id: 14, name: 'Cebula' },
+  { id: 15, name: 'American' },
+  { id: 16, name: 'Polish' },
+  { id: 17, name: 'Deutch' },
+  { id: 18, name: 'Ninja' },
+  { id: 19, name: 'Magento' },
+  { id: 20, name: 'Rozwalka' }
+];
